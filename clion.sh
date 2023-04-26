828b99fbdb2e3a587ec9e85ebff6c81d11a12231
asalo <aaron.salo@jcatechnologies.com>
2023-04-26 10:58:06 -0500

    added clion and pycharm scripts

---
+++ clion.sh
@@ -0,0 +1,110 @@
+#Setup ros2 and start Clion
+
+BROWN='\033[0;33m'
+RED='\033[1;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LGRAY='\033[0;37m'
+LPURPLE='\033[1;35m'
+NC='\033[0m' # No Color
+
+
+drawPgm2() {
+    echo " ▄████▄   ██▓     ██▓ ▒█████   ███▄    █ ";
+    echo "▒██▀ ▀█  ▓██▒    ▓██▒▒██▒  ██▒ ██ ▀█   █ ";
+    echo "▒▓█    ▄ ▒██░    ▒██▒▒██░  ██▒▓██  ▀█ ██▒";
+    echo "▒▓▓▄ ▄██▒▒██░    ░██░▒██   ██░▓██▒  ▐▌██▒";
+    echo "▒ ▓███▀ ░░██████▒░██░░ ████▓▒░▒██░   ▓██░";
+    echo "░ ░▒ ▒  ░░ ▒░▓  ░░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ";
+    echo "  ░  ▒   ░ ░ ▒  ░ ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░";
+    echo "░          ░ ░    ▒ ░░ ░ ░ ▒     ░   ░ ░ ";
+    echo "░ ░          ░  ░ ░      ░ ░           ░ ";
+    echo "░                                        ";
+    echo -e "${NC}"
+}
+
+drawPgm() {
+    echo " ▄████████  ▄█        ▄█   ▄██████▄  ███▄▄▄▄   ";
+    echo "███    ███ ███       ███  ███    ███ ███▀▀▀██▄ ";
+    echo "███    █▀  ███       ███▌ ███    ███ ███   ███ ";
+    echo "███        ███       ███▌ ███    ███ ███   ███ ";
+    echo "███        ███       ███▌ ███    ███ ███   ███ ";
+    echo "███    █▄  ███       ███  ███    ███ ███   ███ ";
+    echo "███    ███ ███▌    ▄ ███  ███    ███ ███   ███ ";
+    echo "████████▀  █████▄▄██ █▀    ▀██████▀   ▀█   █▀  ";
+    echo "           ▀                                   ";
+    echo -e "${NC}"
+}
+
+chooseColor() {
+    r=$(( ( RANDOM % 8 )  + 1 ))
+    case $r in
+		1)
+            echo -e "${BROWN}"
+			;;
+		2) 
+            echo -e "${RED}"
+			;;
+		3)
+            echo -e "${GREEN}"
+			;;
+		4)
+            echo -e "${BLUE}"
+			;;
+		5)
+            echo -e "${PURPLE}"
+			;;
+		6)
+            echo -e "${LGRAY}"
+			;;
+		7)
+            echo -e "${LPURPLE}"
+			;;
+		*)
+            echo -e "${NC}"
+			;;
+	esac		
+
+}
+startPgm() {
+
+    exec $BASE_DIRECTORY/clion/bin/clion.sh
+}
+
+echo -e "${CYAN}====================${NC}"
+echo -e "${CYAN}Setting up for clion${NC}"
+
+echo ""
+echo -e "${CYAN}Setting up ros2 environment${NC}"
+# setup ros2 environment
+source "/opt/ros/$ROS_DISTRO/setup.bash" --
+exec "$@"
+echo -e "${GREEN}Done!${NC}"
+
+echo ""
+echo -e "${CYAN}Installing libxtst6${NC}"
+apt install libxtst6
+echo -e "${GREEN}Done!${NC}"
+echo""
+echo -e "${GREEN}==============================${NC}"
+echo -e "${RED}Finished setup. Starting clion${NC}"
+echo -e "${GREEN}==============================${NC}"
+
+startPgm & 
+
+x=0
+while :
+do
+    sleep 12
+    chooseColor
+    if [ "$x" -eq 0 ]
+        then
+            drawPgm
+            x=$((x+1))
+    else
+        drawPgm2
+        x=$((x-1))
+    fi
+done
---
+++ pycharm.sh
@@ -0,0 +1,54 @@
+#Setup ros2 and start Clion
+
+BROWN='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+
+drawPgm() {
+echo "   ▄███████▄ ▄██   ▄    ▄████████    ▄█    █▄       ▄████████    ▄████████   ▄▄▄▄███▄▄▄▄   ";
+echo "  ███    ███ ███   ██▄ ███    ███   ███    ███     ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄ ";
+echo "  ███    ███ ███▄▄▄███ ███    █▀    ███    ███     ███    ███   ███    ███ ███   ███   ███ ";
+echo "  ███    ███ ▀▀▀▀▀▀███ ███         ▄███▄▄▄▄███▄▄   ███    ███  ▄███▄▄▄▄██▀ ███   ███   ███ ";
+echo "▀█████████▀  ▄██   ███ ███        ▀▀███▀▀▀▀███▀  ▀███████████ ▀▀███▀▀▀▀▀   ███   ███   ███ ";
+echo "  ███        ███   ███ ███    █▄    ███    ███     ███    ███ ▀███████████ ███   ███   ███ ";
+echo "  ███        ███   ███ ███    ███   ███    ███     ███    ███   ███    ███ ███   ███   ███ ";
+echo " ▄████▀       ▀█████▀  ████████▀    ███    █▀      ███    █▀    ███    ███  ▀█   ███   █▀  ";
+echo "                                                                ███    ███                 ";
+
+}
+
+startPgm() {
+
+    exec $BASE_DIRECTORY/clion/bin/clion.sh
+}
+
+echo -e "${CYAN}====================${NC}"
+echo -e "${CYAN}Setting up for pycharm${NC}"
+
+echo ""
+echo -e "${CYAN}Setting up ros2 environment${NC}"
+# setup ros2 environment
+source "/opt/ros/$ROS_DISTRO/setup.bash" --
+exec "$@"
+echo -e "${GREEN}Done!${NC}"
+
+echo ""
+echo -e "${CYAN}Installing libxtst6${NC}"
+apt install libxtst6
+echo -e "${GREEN}Done!${NC}"
+echo""
+
+echo -e "${GREEN}==============================${NC}"
+echo -e "${BROWN}Finished setup. Starting pycharm${NC}"
+echo -e "${GREEN}==============================${NC}"
+
+startPgm & 
+
+while :
+do 
+    sleep 5
+    drawPgm
+done
+
